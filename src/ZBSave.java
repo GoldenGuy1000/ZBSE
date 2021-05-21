e952fe841f34715eefe6d025ba80916fb85f1d4f
GoldenGuy1000 <GoldenGuy1000@gmail.com>
2021-05-21 16:18:42 -0700

    wrote some epic documentation for what ZBSave does

--- src/ZBSave.java
+++ src/ZBSave.java
@@ -3,18 +3,33 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class ZBSave {
-    public int saveNum;
+    public String saveNum;
     private String ZBDir;
 
-    public ZBSave(int saveNumber, String ZBSaveLoc) {
+    /**
+     * reads the file & initialises the save number/directory of the saves
+     *
+     * @param saveNumber The save number you want to access
+     *                   ex: the save number for save 1 is 00 so put that
+     *                   (it's a string because it's easier & I'm not doing math or anything with it)
+     * @param ZBSaveLoc The directory of where the save data for Zapling Bygone is
+     *                  C:\Users\User\AppData\Local\ZaplingBygone (replace User with your user name)
+     */
+    public ZBSave(String saveNumber, String ZBSaveLoc) {
         saveNum = saveNumber;
         ZBDir = ZBSaveLoc;
     }
 
+    /**
+     * refreshes the data from the file (will remove any unsaved modifications)
+     */
     public void read() {
 
     }
 
+    /**
+     * writes back to the file (will overwrite if the file has changed)
+     */
     public void write() {
 
     }
