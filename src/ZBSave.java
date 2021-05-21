97f46299209daa6a08ae20b93b20615106794084
GoldenGuy1000 <GoldenGuy1000@gmail.com>
2021-05-21 15:58:52 -0700

    added variables: saveNum & ZBDir

--- src/ZBSave.java
+++ src/ZBSave.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class ZBSave {
+    public int saveNum;
+    private String ZBDir;
 
-
-    public ZBSave(int saveNumber) {
-
+    public ZBSave(int saveNumber, String ZBSaveLoc) {
+        saveNum = saveNumber;
+        ZBDir = ZBSaveLoc;
     }
 
     public void read() {
